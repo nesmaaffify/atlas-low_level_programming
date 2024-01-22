00d5a60e01ce5aac6b7343795fa916a2b2051d64
nesmaaffify <nesma.affify@atlasschool.com>
2024-01-22 17:21:48 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
- * main- prints _putchar
- *
- * Return: Always 0 (Success)
- *
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-
-	char c[] = "_putchar";
-	int x;
-
-	for (x = 0; x < 8; x++)
-	{
-		_putchar(c[x]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
