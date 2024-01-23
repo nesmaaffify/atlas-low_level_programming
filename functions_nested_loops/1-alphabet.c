44f49aa11a4395d97e1832ce50a0fa1558cd254b
nesmaaffify <nesma.affify@atlasschool.com>
2024-01-23 00:40:52 +0000

    0

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
+
 /**
-  * print_alphabet - Make the alphabet
-  *
-  * Return: void
-  */
+ *  print_alphabet - Make the alphabet
+ *  Return: void
+ */
+
 void print_alphabet(void)
+
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (c= 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
+	
+		putchar(c);
+	
 	}
-
-	_putchar('\n');
+	putchar('\n');
 }
