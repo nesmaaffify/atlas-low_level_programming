fda5afb89c6afe6d1afc2c858d1f056f0c0981c9
nesmaaffify <nesma.affify@atlasschool.com>
2024-01-23 01:08:06 +0000

    4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print a to z
+ *
+ */
+
+void print_alphabet(void)
+{
+	int a = 97;
+	int z = 122;
+	int nn;
+
+	for (nn = a; nn <= z; nn++)
+	{
+		_putchar(nn);
+	}
+	_putchar('\n');
+}
+
+/**
+ * print_alphabet_x10 - print the alphabet ten times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int nn;
+
+	for (nn = 1; nn <= 10; nn++)
+		print_alphabet();
+
+}
+
+/**
+ * _islower - tells if an ascii is a lowercase or not
+ *
+ * @c: a letter or something.
+ *
+ * Return: 1 if lowercase
+ *         0 is uppercase
+ */
+
+int _islower(int c)
+{
+	int a = 97;
+	int z = 122;
+
+	if (c >= a && c <= z)
+		return (1);
+	else
+		return (0);
+
+}
+
+/**
+ * _isalpha - tells if an ascii is a letter or not
+ *
+ * @c: a letter or something.
+ *
+ * Return: 1 if a letter
+ *         0 not a letter
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+
+}
