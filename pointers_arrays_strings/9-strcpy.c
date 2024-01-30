192122098294dfbbdd61e33396bb8ef886af602d
nesmaaffify <nesma.affify@atlasschool.com>
2024-01-30 00:23:56 +0000

    9

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,24 @@
 #include "main.h"
+
 /**
-* _strcpy - Copy paste string
-*@dest: destination
-*@src: source
-*Return: dest
-*/
-char *_strcpy(char *dest, char *src)
+ * puts2 - Prints every other character of a string, starting with the first
+ * @str: Pointer to the input string
+ *
+ * Description: This function prints every other character of the input
+ * string, starting with the first character, followed by a new line.
+ */
+void puts2(char *str)
 {
-int inc = 0;
-while (*(src + inc) != '\0')
-{
-*(dest + inc) = *(src + inc);
-inc++;
-}
-*(dest + inc) = '\0';
-return (dest);
+	unsigned int i = 0;
+	unsigned int limit = 446; /* Adjust this limit as needed */
+
+	while (str[i] != '\0' && i < limit)
+	{
+		/* Print characters at even indices */
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	/* Print a new line at the end */
+	_putchar('\n');
 }
